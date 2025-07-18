456bdee36031f5fb4bfd20cecbde4557ee25c592
Etienne <etienne412@hotmail.com>
2025-07-18 17:38:35 -0400

    Launcher -> Levelx setup ++ Main Menu template

--- KenneyJam2025/Assets/Scripts/MainMenuManager.cs
+++ KenneyJam2025/Assets/Scripts/MainMenuManager.cs
@@ -1,16 +1,81 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("UI References")]
+    public GameObject mainMenuCanvas;
+    public GameObject levelSelectionCanvas;
+    
+    [Header("Main Menu Buttons")]
+    public Button playButton;
+    public Button quitButton;
+
+    [Header("Level Selection Buttons")]
+    public Button backButton;
+    
     void Start()
     {
-        
+        SetupButtonListeners();
+        ShowMainMenu();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LaunchScene(int index)
     {
-        
+        if (GameManager.Instance == null)
+        {
+            Debug.LogError("GameManager not there");
+            return;
+        }
+            
+        string sceneName = "Level" + index;
+        GameManager.Instance.LoadScene(sceneName);
+    }
+    
+    private void SetupButtonListeners()
+    {
+        if (playButton != null)
+            playButton.onClick.AddListener(OnPlayButtonClicked);
+        if (quitButton != null)
+            quitButton.onClick.AddListener(OnQuitButtonClicked);
+        if (backButton != null)
+            backButton.onClick.AddListener(OnBackButtonClicked);
+    }
+
+    private void OnPlayButtonClicked()
+    {
+        ShowLevelSelection();
+    }
+
+    private void OnQuitButtonClicked()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    private void OnBackButtonClicked()
+    {
+        ShowMainMenu();
+    }
+
+    private void ShowMainMenu()
+    {
+        if (mainMenuCanvas != null)
+            mainMenuCanvas.SetActive(true);
+            
+        if (levelSelectionCanvas != null)
+            levelSelectionCanvas.SetActive(false);
+    }
+
+    private void ShowLevelSelection()
+    {
+        if (mainMenuCanvas != null)
+            mainMenuCanvas.SetActive(false);
+            
+        if (levelSelectionCanvas != null)
+            levelSelectionCanvas.SetActive(true);
     }
 }
